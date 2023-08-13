4e0fd5027c206a3605fed10948c56e90735a7bca
kenilshahh <kenilshah1505@gmail.com>
2023-08-13 15:04:42 +0530

    final push

--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -16,7 +16,7 @@ function Todo() {
   }, []);
   const fetchUserData = async () => {
     await fetch(
-      'https://testnet.polybase.xyz/v0/collections/pk%2F0x19048fb81d19b5e3285d29b583b9a21bdad8b423280bea15e80a2488d9ab2f2d471c9d1d84acf2e32ae0fcd62a6916b25def11fc9cbe40df3c6f5f8e8a1f458a%2FPolybaseXChainlink%2FUser/records'
+      'https://testnet.polybase.xyz/v0/collections/pk%2F0x19048fb81d19b5e3285d29b583b9a21bdad8b423280bea15e80a2488d9ab2f2d471c9d1d84acf2e32ae0fcd62a6916b25def11fc9cbe40df3c6f5f8e8a1f458a%2FToDo%2FUser/records'
     )
       .then(async (response) => {
         const dataRes = await response.json();
@@ -452,7 +452,7 @@ function Todo() {
     const someHex = '0x';
     const requestTx = await consumerContract.executeRequest(
       `const url =
-  "https://testnet.polybase.xyz/v0/collections/pk%2F0x19048fb81d19b5e3285d29b583b9a21bdad8b423280bea15e80a2488d9ab2f2d471c9d1d84acf2e32ae0fcd62a6916b25def11fc9cbe40df3c6f5f8e8a1f458a%2FPolybaseXChainlink%2FUser/records"
+  "https://testnet.polybase.xyz/v0/collections/pk%2F0x19048fb81d19b5e3285d29b583b9a21bdad8b423280bea15e80a2488d9ab2f2d471c9d1d84acf2e32ae0fcd62a6916b25def11fc9cbe40df3c6f5f8e8a1f458a%2FToDo%2FUser/records"
 
 const countryRequest = Functions.makeHttpRequest({
   url: url,
